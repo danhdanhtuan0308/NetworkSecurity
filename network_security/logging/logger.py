1128faf5148ecaf7faece96226fa5300e249d9e8
danhdanhtuan0308 <danhdanhtuan0308@gmail.com>
2025-03-25 15:50:38 -0400

    logger

--- network_security/logging/logger.py
+++ network_security/logging/logger.py
@@ -6,7 +6,16 @@ This module is used to create a logger object that can be used to log messages t
 """
 LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
-os.makedirs(logs_path , exist_ok=True)
+# Define the logs directory (not the full file path yet)
+log_dir = os.path.join(os.getcwd(), 'logs')
+os.makedirs(log_dir, exist_ok=True)
 
-LOG_FILE_PATh = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
+
+
+#Log config 
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO
+    )
